cb7fb5be493f23a8a7d33d7cbf6f1d346c5bb653
kiranmanojkumar <manojkiran80@gmail.com>
2019-01-12 17:53:00 -0500

    First commit of project

---
+++ app/src/main/java/com/hackathon/hackathon/ClientActivity.java
@@ -0,0 +1,18 @@
+package com.hackathon.hackathon;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class ClientActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_client);
+    }
+
+
+
+}
---
+++ app/src/main/java/com/hackathon/hackathon/OrganizationActivity.java
@@ -0,0 +1,37 @@
+package com.hackathon.hackathon;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class OrganizationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_organization);
+         final EditText test = (EditText)findViewById(R.id.test);
+
+
+        Timer time =new Timer();
+        time.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                runOnUiThread(new Runnable()
+                {
+                    public void run()
+                    {
+                        test.setText(""+Math.random());
+                    }
+                });
+            }
+        }, 1000, 1000); // does this task every second
+    }
+
+
+
+
+}
