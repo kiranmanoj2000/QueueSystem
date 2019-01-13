c2d47be4572418b0ddebf44a746ffaa292069d08
Frank <fancyfrank30@gmail.com>
2019-01-13 02:00:06 -0500

    QR code related

--- app/src/main/java/com/hackathon/hackathon/ClientActivity.java
+++ app/src/main/java/com/hackathon/hackathon/ClientActivity.java
@@ -1,17 +1,101 @@
 package com.hackathon.hackathon;
 
-import android.content.Intent;
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Vibrator;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
+import android.util.SparseArray;
+
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.TextView;
+
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
+
+import java.io.IOException;
+
 
 public class ClientActivity extends AppCompatActivity {
+
+    SurfaceView surfaceView;
+    CameraSource cameraSource;
+    TextView textView;
+    BarcodeDetector barcodeDetector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_client);
+        surfaceView=(SurfaceView)findViewById(R.id.camerapreview);
+        textView=(TextView)findViewById(R.id.textView);
+
+        barcodeDetector=new BarcodeDetector.Builder(this).setBarcodeFormats(Barcode.QR_CODE).build();
+
+        cameraSource = new CameraSource.Builder(this,barcodeDetector)
+                .setRequestedPreviewSize(640,490).build();
+
+        surfaceView.getHolder().addCallback(new SurfaceHolder.Callback() {
+
+            @Override
+            public void surfaceCreated(SurfaceHolder holder){
+                if (ActivityCompat.checkSelfPermission(getApplicationContext(),Manifest.permission.CAMERA)!= PackageManager.PERMISSION_GRANTED){
+                    return;
+                }
+                try{
+                    cameraSource.start(holder);
+                }catch(IOException e)
+                {
+                    e.printStackTrace();
+                }
+
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height){
+
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder){
+                cameraSource.stop();
+            }
+
+        });
+
+        barcodeDetector.setProcessor(new Detector.Processor<Barcode>(){
+            @Override
+            public void release(){
+
+            }
+
+            public void receiveDetections(Detector.Detections<Barcode> detections){
+                final SparseArray<Barcode> qrCodes=detections.getDetectedItems();//sketchy addition of final
+
+                if (qrCodes.size()!=0){
+                    textView.post(new Runnable(){
+                        @Override
+                        public void run(){
+                            Vibrator vibrator=(Vibrator)getApplicationContext().getSystemService(Context.VIBRATOR_SERVICE);
+                            vibrator.vibrate(1000);
+                            textView.setText(qrCodes.valueAt(0).displayValue);
+                        }
+                    });
+                }
+            }
+
+
+        });
+
+
+
     }
+
 
 
 
