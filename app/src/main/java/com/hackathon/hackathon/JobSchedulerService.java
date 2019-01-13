ed80c40b98f16696cd73f90311f371fd80ec03ee
kiranmanojkumar <manojkiran80@gmail.com>
2019-01-13 09:23:22 -0500

    Made several changes

--- app/src/main/java/com/hackathon/hackathon/JobSchedulerService.java
+++ app/src/main/java/com/hackathon/hackathon/JobSchedulerService.java
@@ -1,22 +1,34 @@
 package com.hackathon.hackathon;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.job.JobParameters;
 import android.app.job.JobService;
+import android.os.Process;
+import android.support.annotation.NonNull;
+import android.support.v4.app.NotificationCompat;
+import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class JobSchedulerService extends JobService {
     private boolean jobWorking = false;
     public boolean jobCancel = false;
 
     @Override
-    public boolean onStartJob(JobParameters parameters){
+    public boolean onStartJob(final JobParameters parameters){
         jobWorking = true;
-
-        FirebaseDatabase.getInstance().getReference("Background").setValue("YEEEET");
 
 
         // job logic
+
+        runOnThread(parameters);
+        jobFinished(parameters, false);
+
         return jobWorking;
     }
 
@@ -29,6 +41,60 @@ public class JobSchedulerService extends JobService {
         jobFinished(parameters, reschedule);
         // let the system know if it needs to be reschedueled
         return reschedule;
+    }
+
+  //  public boolean checkIfTurn(){
+       // if()
+    //}
+
+    public void runOnThread(final JobParameters parameters){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                // set the thread in the background
+                android.os.Process.setThreadPriority(Process.THREAD_PRIORITY_MORE_FAVORABLE);
+                try {
+                    // event listener for the database
+                    FirebaseDatabase.getInstance().getReference("CurrentQueueInLine").addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                            // it is their turn
+                            if((long)dataSnapshot.getValue() == parameters.getExtras().getLong("clientID")){
+                                notifyClient();
+                            }
+
+
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                        }
+                });
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+
+
+
+            }
+
+
+        }).start();
+    }
+
+    public void notifyClient(){
+        try {
+            NotificationChannel channel = new NotificationChannel("Channel_1", "Notify", NotificationManager.IMPORTANCE_HIGH);
+            NotificationManager notifyManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+            notifyManager.createNotificationChannel(channel);
+            Notification notify = new NotificationCompat.Builder(this)
+                    .setSmallIcon(R.drawable.ic_launcher_foreground).setContentTitle("Your Seat is Ready!").setContentText("Come On In!")
+                    .setChannelId("Channel_1").build();
+            notifyManager.notify((int) (Math.random()*1001), notify);
+        }catch (Exception error){
+            error.printStackTrace();
+        }
     }
 }
 
--- app/src/main/java/com/hackathon/hackathon/OrganizationActivity.java
+++ app/src/main/java/com/hackathon/hackathon/OrganizationActivity.java
@@ -25,33 +25,47 @@ import java.util.TimerTask;
 public class OrganizationActivity extends AppCompatActivity {
 
     int lineNum = 0;
+    private int queueSize = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_organization);
-         final EditText test = (EditText)findViewById(R.id.test);
 
-         ArrayList<String> tests = new ArrayList<>();
-         tests.add("Hello");
-         tests.add("arrau");
-         tests.add("stack");
-         FirebaseDatabase.getInstance().getReference("CurrentQueue").setValue(1);
-         FirebaseDatabase.getInstance().getReference("User2").setValue(2);
-         FirebaseDatabase.getInstance().getReference("User3").setValue(3);
-         FirebaseDatabase.getInstance().getReference("User4").setValue(tests);
-         JobScheduler scheduler = (JobScheduler) this.getSystemService(Context.JOB_SCHEDULER_SERVICE);
+         FirebaseDatabase.getInstance().getReference("CurrentQueueInLine").setValue(0);
+         FirebaseDatabase.getInstance().getReference("CurrentQueueSize").setValue(0);
 
-         ComponentName service = new ComponentName(this, JobSchedulerService.class);
-         JobInfo inf = new JobInfo.Builder(2, service).setMinimumLatency(1000).build();
-         scheduler.schedule(inf);
 
-        FirebaseDatabase.getInstance().getReference("CurrentQueue").addValueEventListener(new ValueEventListener() {
+        // when the current queue changes update the UI
+        FirebaseDatabase.getInstance().getReference("CurrentQueueInLine").addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 long number = (long)dataSnapshot.getValue();
-                TextView displayNum = (TextView)findViewById(R.id.textView);
-                displayNum.setText(String.valueOf(number));
+                // if the number is zero
+                if(number==0){
+                    TextView displayNum = (TextView)findViewById(R.id.textView);
+                    displayNum.setText("No More Customers");
+                }else{
+                    TextView displayNum = (TextView)findViewById(R.id.textView);
+                    displayNum.setText(String.valueOf(number));
+                }
+
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        FirebaseDatabase.getInstance().getReference("CurrentQueueSize").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                long size = (long)dataSnapshot.getValue();
+                TextView sizeQueue = (TextView)findViewById(R.id.numQueue);
+                sizeQueue.setText(size+"");
             }
 
             @Override
@@ -61,26 +75,26 @@ public class OrganizationActivity extends AppCompatActivity {
         });
 
 
-
-         Timer time = new Timer();
-         time.scheduleAtFixedRate(new TimerTask() {
-            @Override
-            public void run() {
-                runOnUiThread(new Runnable()
-                {
-                    public void run()
-                    {
-                        test.setText(""+Math.random());
-                    }
-                });
-            }
-        }, 1000, 1000); // does this task every second
     }
 
-    public void decreaseQueue(View view) {
+    // will be called when a customer leaves
+    public void moveToNextInLine(View view) {
         ++lineNum;
-        FirebaseDatabase.getInstance().getReference("CurrentQueue").setValue(lineNum);
+        ++queueSize;
+        FirebaseDatabase.getInstance().getReference("CurrentQueueInLine").setValue(lineNum);
+        // increase the size
+
     }
+
+    // for closing up shop
+    public void closeQueueing(View view){
+        FirebaseDatabase.getInstance().getReference("CurrentQueueInLine").setValue(0);
+
+    }
+
+    // when
+
+
 
 
 
